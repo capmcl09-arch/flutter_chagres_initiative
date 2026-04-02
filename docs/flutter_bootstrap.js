6491929d86363543c43b0a6d68bbbcc0651c50f0
capmcl09-arch <capmcl09@gmail.com>
2026-04-01 22:46:48 -0500

    Rebuild Flutter web with correct base href for custom domain deployment

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "874027543" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2245346533" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
