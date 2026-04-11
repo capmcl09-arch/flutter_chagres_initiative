6dcd77df275b6035d821206761636de2a7121521
capmcl09-arch <capmcl09@gmail.com>
2026-04-11 16:12:35 -0500

    Clean rebuild and redeploy

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1984039919" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1710579088" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
