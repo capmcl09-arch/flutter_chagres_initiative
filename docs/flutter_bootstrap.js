59fb3aae4d4469179f6bf5b0d49175c418c00b3f
capmcl09-arch <capmcl09@gmail.com>
2026-04-11 15:14:17 -0500

    Rebuild web with new logo changes and update deployed docs

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "411079081" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3321702225" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
